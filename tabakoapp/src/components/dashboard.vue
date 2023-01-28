385e9ac919ab4b3b4c850d23f471c95ef08137eb
doraemon123453@gmail.com <doraemon123453@gmail.com>
2023-01-29 08:51:52 +0900

    dashboardAPI取得完了

--- tabakoapp/src/components/dashboard.vue
+++ tabakoapp/src/components/dashboard.vue
@@ -57,7 +57,135 @@
   import trivia from './trivia.vue'
   import report from './report.vue'
   import graph from './graph.vue'
-  
+  import {reactive} from "vue"
+  import axios from 'axios'
+  let today=reactive({
+    num:0,
+    money:0,
+    tax:0,
+    tar:0,
+    nicotine:0,
+    time:1040,
+    lifespan:"",
+    if_money:"",
+    if_story:"",
+  })
+  let all=reactive({
+    num:0,
+    money:0,
+    tax:0,
+    tar:0,
+    nicotine:0,
+    time:1040,
+    lifespan:"",
+    if_money:"",
+    if_story:"",
+  })
+  let this_month=reactive({
+    num:0,
+    money:0,
+    tax:0,
+    tar:0,
+    nicotine:0,
+    time:1040,
+    lifespan:"",
+    if_money:"",
+    if_story:"",
+  })
+  let one_month=reactive({
+    num:0,
+    money:0,
+    tax:0,
+    tar:0,
+    nicotine:0,
+    time:1040,
+    lifespan:"",
+    if_money:"",
+    if_story:"",
+  })
+  let two_month=reactive({
+    num:0,
+    money:0,
+    tax:0,
+    tar:0,
+    nicotine:0,
+    time:1040,
+    lifespan:"",
+    if_money:"",
+    if_story:"",
+  })
+  axios
+    .post('http://mp-class.chips.jp/tobaco/main.php', {
+        user_id:1,//ユーザID
+        login_user: ''
+    }, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    })
+    .then(function (res) {
+        
+        let resall=res.data.tobaco.all;
+        console.log(res.data)
+        all.num=resall.num;
+        all.money=resall.money;
+        all.tax=resall.tax;
+        all.tar=resall.tar;
+        all.nicotine=resall.nicotine;
+        all.time=resall.time;
+        all.lifespan=resall.lifespan;
+        all.if_money=resall.if.if_money;
+        all.if_story=resall.if.if_story;
+
+        resall=res.data.tobaco.today;
+        today.num=resall.num;
+        today.money=resall.money;
+        today.tax=resall.tax;
+        today.tar=resall.tar;
+        today.nicotine=resall.nicotine;
+        today.time=resall.time;
+        today.lifespan=resall.lifespan;
+        today.if_money=resall.if.if_money;
+        today.if_story=resall.if.if_story;
+
+        resall=res.data.tobaco.this_morth;
+        this_month.num=resall.num;
+        this_month.money=resall.money;
+        this_month.tax=resall.tax;
+        this_month.tar=resall.tar;
+        this_month.nicotine=resall.nicotine;
+        this_month.time=resall.time;
+        this_month.lifespan=resall.lifespan;
+        this_month.if_money=resall.if.if_money;
+        this_month.if_story=resall.if.if_story;
+
+        resall=res.data.tobaco.one_morth;
+        one_month.num=resall.num;
+        one_month.money=resall.money;
+        one_month.tax=resall.tax;
+        one_month.tar=resall.tar;
+        one_month.nicotine=resall.nicotine;
+        one_month.time=resall.time;
+        one_month.lifespan=resall.lifespan;
+        one_month.if_money=resall.if.if_money;
+        one_month.if_story=resall.if.if_story;
+
+        resall=res.data.tobaco.two_morth;
+        two_month.num=resall.num;
+        two_month.money=resall.money;
+        two_month.tax=resall.tax;
+        two_month.tar=resall.tar;
+        two_month.nicotine=resall.nicotine;
+        two_month.time=resall.time;
+        two_month.lifespan=resall.lifespan;
+        two_month.if_money=resall.if.if_money;
+        two_month.if_story=resall.if.if_story;
+        console.log(today)
+        console.log(this_month)
+        console.log(one_month)
+        console.log(two_month)
+        console.log(all)
+    })
   </script>
   <style scoped>
   .wrap{
